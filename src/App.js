4e716c6701822e8f82887c6c6f5615841221989a
datau-hykim <hykim@datau.co.kr>
2023-12-23 16:57:45 +0900

    route 오류 수정

--- src/App.js
+++ src/App.js
@@ -1,55 +1,67 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import Movie from './components/Movie';
 import MovieForm from './components/MovieForm';
 import Navbar from './components/Navbar';
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
+    BrowserRouter as Router,
+    Switch,
+    Route, Routes,
 } from 'react-router-dom';
+
 function App() {
-  const [movies, setMovies] = useState([]);
+    const [movies, setMovies] = useState([]);
 
-  const removeMovie = (id) => {
-      setMovies(movies.filter(movie => {
-        return movie.id !== id;
-      }));
-  };
+    const removeMovie = (id) => {
+        setMovies(movies.filter(movie => {
+            return movie.id !== id;
+        }));
+    };
 
-  const renderMovies = movies.length ? movies.map(movie => {
+    const renderMovies = movies.length ? movies.map(movie => {
+        return (
+            <Movie
+                movie={movie}
+                key={movie.id}
+                removeMovie={removeMovie}
+            />
+        );
+    }) : '추가된 영화가 없습니다';
+    const addMovie = (movie) => {
+        setMovies([
+            ...movies,
+            movie
+        ]);
+    };
     return (
-      <Movie 
-        movie={movie} 
-        key={movie.id}
-        removeMovie={removeMovie}
-      />
+        <Router>
+            <div className="App">
+                <Navbar/>
+                <Routes>
+                    {/*요즘에는 이렇게 사용안하는데 일단 이렇게 해서 완강해봐 알려줄게 나중에*/}
+                    <Route path="/movies" element={
+                        <>
+                            <h1> Movie list</h1>
+                            <MovieForm addMovie={addMovie}/>
+                            {renderMovies}
+                        </>
+                    }>
+                    </Route>
+                    <Route path="/users" elemet={
+                        <>
+                            <h1>Users</h1>
+                        </>
+                    }>
+                    </Route>
+                    <Route path="/" element={
+                        <>
+                            <h1>Home</h1>
+                        </>
+                    }>
+                    </Route>
+                </Routes>
+            </div>
+        </Router>
     );
-  }) : '추가된 영화가 없습니다';
-  const addMovie= (movie) => {
-    setMovies([
-      ...movies, 
-      movie
-    ]);
-  };
-  return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <Route path="/movies">
-          <h1> Movie list</h1>
-          <MovieForm addMovie={addMovie} />
-          {renderMovies}
-        </Route> 
-        <Route path="/users">
-          <h1>Users</h1>
-        </Route>
-        <Route path="/">
-          <h1>Home</h1>
-        </Route>
-        
-      </div>
-    </Router>
-  );
 }
 
 
